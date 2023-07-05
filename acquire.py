65246bee4117b3c2647545868cc7a706907a7d25
Oliver k Taylor <o.k.p.taylor@gmail.com>
2023-07-05 01:12:35 -0500

    added acquire.py

---
+++ acquire.py
@@ -0,0 +1,98 @@
+#Import dependencies
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import os
+import json
+
+
+headers = {'User-Agent': 'Codeup Data Science'}
+
+#scrape blog homepage for links
+
+url = "https://codeup.com/blog/"
+
+response = get(url, headers = headers)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+more_links = soup.find_all('a', class_="more-link")
+
+#extract the links into a list
+
+links_list = [link['href'] for link in more_links]
+
+def get_blog_articles(article_list=links_list):
+    """
+    
+    """
+    file = "blog_posts.json"
+    
+    if os.path.exists(file):
+        with open(file) as f:
+            return json.load(f)
+        
+    
+    headers = {'User-Agent': 'Codeup Data Science'}
+    article_info = []
+
+    for article in article_list:
+        response = get(article, headers=headers)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        info_dict = {"title":soup.find("h1").text,
+                    "link": article,
+                    "date_published":soup.find('span', class_="published").text,
+                    "content": soup.find('div', class_="entry-content").text}
+        article_info.append(info_dict)
+    
+    with open(file, "w") as f:
+        json.dump(article_info, f)
+    return article_info
+
+
+
+def scrape_one_page(topic):
+#topic = "business"
+    base_url = "https://inshorts.com/en/read/"
+
+    response = get(base_url + topic)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    titles = soup.find_all('span', itemprop="headline")
+
+    summaries = soup.find_all('div', itemprop="articleBody")
+
+    summary_list = []
+
+    for i in range(len(titles)):
+        temp_dict = {"category": topic,
+                    "title": titles[i].text,
+                    "content": summaries[i].text}
+
+        summary_list.append(temp_dict)
+
+    return summary_list
+
+def get_news_articles(topic_list):
+    """
+    
+    """
+    file = "news_articles.json"
+    if os.path.exists(file):
+        with open(file) as f:
+            return json.load(f)
+
+    final_list = []
+
+    for topic in topic_list:
+        final_list.extend(scrape_one_page(topic))
+    
+    with open(file, "w") as f:
+        json.dump(final_list, f)
+        
+    return final_list
+
